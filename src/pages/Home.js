9823bfed684cbddd64fb9ca0712b1ae817bc1e63
LWIZN <81065913+LWIZN@users.noreply.github.com>
2023-03-08 12:40:16 +0800

    03. 08 2023 1240

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import Carousel from "../components/Carousel";
 export default function Home() {
   return (
     <div className="home_background">
+      <div className="welcome_container">
+        <h1>Welcome To Our Studio</h1>
+      </div>
       <div className="home_frame">
         <div className="image_container">
           <Carousel />
